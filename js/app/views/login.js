977007c2aff23a4872b10ff25deae43120ddce47
matbrady <mat.brady@threespot.com>
2013-07-30 13:27:57 -0400

    adding email input validation

--- js/app/views/login.js
+++ js/app/views/login.js
@@ -8,7 +8,8 @@ define('views/login', ['jquery', 'backbone', 'underscore'], function($, Backbone
 
 		events: {
 			'click #submit': 'submit',
-			'keyup input': 'enableSubmitBtn'
+			'keyup input': 'enableSubmitBtn',
+			'blur input': 'validateOffFocus'
 		},
 
 		initialize: function() {
@@ -19,6 +20,7 @@ define('views/login', ['jquery', 'backbone', 'underscore'], function($, Backbone
 		},
 
 
+		// Validate the inputs for values
 		enableSubmitBtn: function( evt ) {
 			// console.log(evt.which);
 			if ( !!this.$email.val().length && !!this.$password.val().length ) {
@@ -26,6 +28,26 @@ define('views/login', ['jquery', 'backbone', 'underscore'], function($, Backbone
 			} else {
 				this.$submit.attr('disabled', 'disabled');
 			}
+		},
+
+
+		//
+		validateOffFocus: function( evt ) {
+			switch ( evt.target.id ) {
+				case 'email':
+					var isValid = this.validateEmail( this.$email.val() );
+					console.log(isValid);
+				break;
+				case 'password':
+				break;
+			}
+		},
+
+
+		// Validate Email Address
+		validateEmail: function( email ) { 
+		    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		    return re.test(email);
 		},
 
 
